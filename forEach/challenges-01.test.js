49cde2e92df68fbd87c6c9518a2298e769164193
Carlos Cadena <cs.cadena@gmail.com>
2018-09-04 17:50:05 -0700

    challenge 6 passing

--- forEach/challenges-01.test.js
+++ forEach/challenges-01.test.js
@@ -123,10 +123,17 @@ const addNumbers = (num, arr, times, callback) => {
 
 const removeOne = (num, input) => {
   // Solution code here...
+  if (num % 3 === 2){
+      input.pop()
+  }
 }
 
 const removeElements = (input, callback) => {
   // Solution code here...
+  for(let i of input){
+      callback(i, input)
+  }
+  return input
 }
 
 // ------------------------------------------------------------------------------------------------
